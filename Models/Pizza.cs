bc619b038d186b3cf0654b40219d493334756096
Cristian <sanchez.cn@hotmail.com>
2022-11-15 09:50:29 -0300

    update code

--- Models/Pizza.cs
+++ Models/Pizza.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace ContosoPizza.Models;
 
 public class Pizza
@@ -5,4 +7,10 @@ public class Pizza
     public int Id { get; set; }
     public string? Name { get; set; }
     public bool IsGlutenFree { get; set; }
+}
+
+class PizzaDb : DbContext
+{
+    public PizzaDb(DbContextOptions options) : base(options) { }
+    public DbSet<Pizza> Pizzas { get; set; } = null!;
 }
--- Program.cs
+++ Program.cs
@@ -1,11 +1,25 @@
+using Microsoft.OpenApi.Models;
+using Microsoft.EntityFrameworkCore;
+using ContosoPizza.Services;
+using ContosoPizza.Models;
+
+
 var builder = WebApplication.CreateBuilder(args);
+
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<PizzaDb>(options => options.UseInMemoryDatabase("items"));
+builder.Services.AddSwaggerGen(c =>
+{
+     c.SwaggerDoc("v1", new OpenApiInfo {
+         Title = "PizzaStore API",
+         Description = "Making the Pizzas you love",
+         Version = "v1" });
+});
 
 var app = builder.Build();
 
@@ -13,13 +27,16 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "PizzaStore API V1");
+    });
 }
-
+app.MapGet("/", () => "Hello World!");
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
-
+app.MapGet("/pizzas", async (PizzaDb db) => await db.Pizzas.ToListAsync());
 app.Run();
